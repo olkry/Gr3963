ec74ee6b6ea707083b893b0f02ebacf95f7a265c
olkry <adenos90@gmail.com>
2023-02-12 13:08:04 +0300

    add solution for task 2,4,6,8

---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB)
+{
+    if (numA > numC)
+    {
+        Console.WriteLine("Большее число - " + numA);
+    }
+    else
+    {
+        Console.WriteLine("Большее число - " + numC);
+    }
+}
+else
+{
+    if(numB>numC)
+    {
+        Console.WriteLine("Большее число - " + numB);
+    }
+    else
+    {
+        Console.WriteLine("Большее число - " + numC);
+    }
+}
---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите  число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 2; i < num; i = i + 2)
+{
+    Console.Write(i + " ");
+}
