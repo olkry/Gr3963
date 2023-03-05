b4e1e1909b2371d33cb7ce50028a383a6821dda0
olkry <adenos90@gmail.com>
2023-03-05 19:35:15 +0300

    Add solution for task 52

--- Sem7Task52/Program.cs
+++ Sem7Task52/Program.cs
@@ -3,67 +3,77 @@
 
 int ReadData(string msg)
 {
-      Console.Write(msg);
-      return int.Parse(Console.ReadLine() ?? "0");
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void Print1DArr(double[] arr)
+{
+    Console.WriteLine("Среднее арифметическое значение столбцов:");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + "\t" + "|");
+    }
+    Console.WriteLine(arr[arr.Length - 1]);
 }
 //генерация 2мерного массива под задачу
 int[,] Gen2DArray(int countRow, int countColumn, int min, int max)
 {
-      if (min > max)
-      {
-            int buf = max;
-            max = min;
-            min = buf;
-      }
+    if (min > max)
+    {
+        int buf = max;
+        max = min;
+        min = buf;
+    }
 
-      Random rnd = new Random();
-      int[,] arr = new int[countRow, countColumn];
-      for (int i = 0; i < countRow; i++)
-      {
-            for (int j = 0; j < countColumn; j++)
-            {
-                  arr[i, j] = rnd.Next(min, max + 1);
-            }
-      }
-      return arr;
+    Random rnd = new Random();
+    int[,] arr = new int[countRow, countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
 }
 // Вывод 2мерного массива 
 void Print2DArr(int[,] arr)
 {
 
-      for (int i = 0; i < arr.GetLength(0); i++)
-      {
-            for (int j = 0; j < arr.GetLength(1); j++)
-            {
-                  Console.Write(arr[i, j] + " ");
-            }
-            Console.WriteLine();
-      }
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1)-1; j++)
+        {
+            Console.Write(arr[i, j] + "\t" + "|");
+        }
+        Console.WriteLine(arr[i, arr.GetLength(1)-1]);
+        Console.WriteLine();
+    }
+    
 }
 
-void Print1DArr(double[] arr)
-{
-      Console.Write("[");
-      for (int i = 0; i < arr.Length - 1; i++)
-      {
-            Console.Write(arr[i] + ", ");
-      }
-      Console.Write(arr[arr.Length - 1]);
-      Console.WriteLine("]");
-}
 //Метод подсчёта среднеарефметич в колоннах
-double[] ColMean2DArr(int[,] arr);
+double[] ColMean2DArr(int[,] arr)
 {
-      double[] resArr = new double[arr.GetLength(1)];
-      for (int j = 0; j < arr.GetLength(1); j++)
-      {
-            for (int i = 0; i < arr.GetLength(0); i++)
-            {
-                  resArr[j] += arr[i, j];
-            }
-            resArr[j] = resArr[j] / arr.GetLength(0);
-      }
+    double[] resArr = new double[arr.GetLength(1)];
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            resArr[j] += arr[i, j];
+        }
+        resArr[j] = resArr[j] / arr.GetLength(0);
+        resArr[j] = Math.Round(resArr[j], 2);
+    }
 
-      return resArr;
+    return resArr;
 }
 
+int row = ReadData("Введите колличество строк: ");
+int column = ReadData("Введите колличество столбцов: ");
+int[,] arrey = Gen2DArray(row, column, -999, 999);
+Print2DArr(arrey);
+double[] arrSrAr = ColMean2DArr(arrey);
+Console.WriteLine();
+Print1DArr(arrSrAr);
