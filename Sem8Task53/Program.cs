1d4178a5850a399b898f64a39ab8842c34a95195
olkry <adenos90@gmail.com>
2023-03-09 18:40:46 +0300

    Add soluyion for tasks 53,61

---
+++ Sem8Task53/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю
+// строку массива.
+
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//генерация 2мерного массива приёмом массива
+void Gen2DArray(int[,] matr, int min, int max)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+}
+// Вывод 2мерного массива 
+void Print2DArr(int[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+//Метод замены 1ой и последней строки местамию
+void Chenge2DArray(int[,] matr)
+{
+    int temp = 0;
+    for (int i = 0; i < matr.GetLength(1); i++)
+    {
+        //(matr[0,1],matr[matr.GetLength(0)-1,i])=(matr[matr.GetLength(0)-1,i]);
+        temp = matr[0, i];
+        matr[0, i] = matr[matr.GetLength(0) - 1, i];
+        matr[matr.GetLength(0) - 1, i] = temp;
+    }
+}
+int m = ReadData("Введите количество строк: ");
+int n = ReadData("Введите количество столбцов: ");
+int[,] matrix = new int[m, n];
+Gen2DArray(matrix,1,100);
+Print2DArr(matrix);
+Console.WriteLine();
+Chenge2DArray(matrix);
+Print2DArr(matrix);
---
+++ Sem8Task61/Program.cs
@@ -0,0 +1,51 @@
+﻿// Вывести первые N строк треугольника Паскаля.
+// Сделать вывод в виде равнобедренного
+// треугольника
+
+//Ещё вариант ввода
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// n!/k!*(n-k)!
+
+// Метод подсчёта факториала
+long Factorieal(int n)
+{
+    long res = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        res = res * i;
+    }
+    return res;
+}
+
+//     1
+//    1 1
+//   1 2 1
+//  1 3 3 1
+
+//Метод печати триугольника Паскаля
+void PrintPascalTriangle(int nRow)
+{
+    for (int i = 0; i < nRow; i++)
+    {
+        for (int k = 0; k < nRow-i; k++)
+        {
+            Console.Write(" ");
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            Console.Write(" ");
+            Console.Write(Factorieal(i)/(Factorieal(j)*Factorieal(i-j)));
+        }
+        Console.WriteLine();
+    }
+}
+
+int countRow = ReadData("Введите количество строк триугольника Паскаля: ");
+PrintPascalTriangle(countRow);
